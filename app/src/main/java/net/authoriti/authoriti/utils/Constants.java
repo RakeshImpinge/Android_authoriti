5e10041b1d02942b70800cf69881e76d5bada3c8
Rakib Ansary Saikot <ansaryfantastic@gmail.com>
2019-05-04 02:56:49 +0600

    phone number update

--- app/src/main/java/net/authoriti/authoriti/utils/Constants.java
+++ app/src/main/java/net/authoriti/authoriti/utils/Constants.java
@@ -8,7 +8,7 @@ public interface Constants {
     String DEV = "davjnabB8rvyZZLrMOq3jw==:JDJXMckcsxcMhtMi1QVmEs4DpAFtLn9FMZwASUiH9B4=:qeU5Uk2rFKcoMtK0N1XYosOSfhIar0K8Hgk8rIn4BcEdVmyDMOnYBJtEg4P6hZXg";
     String QA = "WSNTO7K0zcMl0pE/kL8L4g==:91JmJhXx2OQPfLr/0aCesZktWi72LMccgjw0zLzkIJg=:J28cEDRsdvjgcFVrtRdE6dUjc5VZUDKPKIKe+UoUm1Y=";
     String PROD = "OUCfUyCJLsvrtuZ+HWMTTA==:Z7psrKA4pogc9O1ZjtGatTPDwpoq6T8hb599+VLZzr0=:SIkVX1tZ6KKTjszqQB0cd/GE/0pqD5OMdpqvvQlNLTA=";
-    String API_BASE_URL = DEV;
+    String API_BASE_URL = QA;
     String API_BASE_URL_POLLING =
             "https://s3.amazonaws.com/authoriti-requests-qa/requests/";
     String HELP_BASE = "https://help.authoriti.net";
@@ -52,9 +52,7 @@ public interface Constants {
     String TIME_CUSTOM_DATE = "Custom Date/Time";
 
     String ACUANT_LICENSE_KEY = "FAD9B2F0E7F1";
-
-    String AUTHORIZE_CALL_NUMBER = "+916283725831";
-
+    String AUTHORIZE_CALL_NUMBER = "512.798.3750";
 
     String TOPIC_PURPOSE_MANAGE_MY_AC = "help-phone-inquiry";
     String TOPIC_PURPOSE_SEND_MONEY = "help-send-cash";
