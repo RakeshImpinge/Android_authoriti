e078fbfc70fea5a0eaf78453b1b477942f1fb1db
Rakib Ansary Saikot <ansaryfantastic@gmail.com>
2019-06-14 00:34:07 +0600

    AU-125

--- app/src/main/java/net/authoriti/authoriti/utils/Constants.java
+++ app/src/main/java/net/authoriti/authoriti/utils/Constants.java
@@ -9,8 +9,7 @@ public interface Constants {
     String QA = "WSNTO7K0zcMl0pE/kL8L4g==:91JmJhXx2OQPfLr/0aCesZktWi72LMccgjw0zLzkIJg=:J28cEDRsdvjgcFVrtRdE6dUjc5VZUDKPKIKe+UoUm1Y=";
     String PROD = "OUCfUyCJLsvrtuZ+HWMTTA==:Z7psrKA4pogc9O1ZjtGatTPDwpoq6T8hb599+VLZzr0=:SIkVX1tZ6KKTjszqQB0cd/GE/0pqD5OMdpqvvQlNLTA=";
     String API_BASE_URL = DEV;
-    String API_BASE_URL_POLLING =
-            "https://s3.amazonaws.com/authoriti-requests-qa/requests/";
+
     String HELP_BASE = "https://help.authoriti.net";
     String IDENTIFIER = "authoriti";
 
